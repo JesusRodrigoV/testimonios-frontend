9ba490d07172a35c48ca6fd780f6bf1212299e7f
JesusRodrigoV <jaicelvelasco105@gmail.com>
2025-04-17 22:04:32 -0400

    feat: Agregados componentes header y footer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,17 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
-import {
-  provideHttpClient,
-  withFetch,
-  withInterceptors,
-} from "@angular/common/http";
-import { AuthInterceptor } from "./core/interceptors/auth.interceptor";
+import { provideHttpClient, withFetch } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch(), withInterceptors([AuthInterceptor])),
+    provideHttpClient(withFetch()),
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,3 @@
 import { Routes } from "@angular/router";
-import { authGuard } from "./core/guards/auth.guard";
 
-export const routes: Routes = [
-  {
-    path: "login",
-    loadComponent: () =>
-      import("./features/auth/login/login.component").then(
-        (m) => m.LoginComponent,
-      ),
-  },
-  {
-    path: "admin",
-    loadChildren: () =>
-      import("./features/admin/admin.routes").then((m) => m.adminRoutes),
-    canActivate: [authGuard],
-    data: { requiredRole: Rol.ADMIN },
-  },
-  {
-    path: "curator",
-    loadChildren: () =>
-      import("./features/curator/curator.routes").then((m) => m.curatorRoutes),
-    canActivate: [authGuard],
-    data: { requiredRole: Rol.CURATOR },
-  },
-];
+export const routes: Routes = [];
