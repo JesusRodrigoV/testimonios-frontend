2eb80acc51efbac41cd6e2737b86befad99cab52
JesusRodrigoV <jaicelvelasco105@gmail.com>
2025-04-17 23:09:49 -0400

    feat: Agregado footer basico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { FooterComponent } from "./components/footer";
+import { HeaderComponent } from "./components/header";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'testimonios-frontend';
+  title = "testimonios-frontend";
 }
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,42 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+
+interface FooterLink {
+  name: string;
+  url: string;
+  icon: string;
+  external?: boolean;
+}
 
 @Component({
-  selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-footer",
+  imports: [CommonModule, RouterModule],
+  templateUrl: "./footer.component.html",
+  styleUrl: "./footer.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {
+  readonly currentYear = new Date().getFullYear();
 
+  readonly brand = {
+    logo: "assets/images/logo.png",
+    title: "Legado de Bolivia",
+    description: "Sistema de Archivos de Testimonios del Bicentenario",
+  };
+
+  readonly links: FooterLink[] = [
+    {
+      name: "GitHub",
+      url: "https://github.com/tu-organizacion/testimonios-frontend",
+      icon: "bx bxl-github",
+      external: true,
+    },
+    {
+      name: "UCB",
+      url: "https://www.ucb.edu.bo/",
+      icon: "bx bx-building-house",
+      external: true,
+    },
+  ];
 }
