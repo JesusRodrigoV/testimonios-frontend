e3b2262e31795dda0a56f9a6074f96b517f4aee8
JesusRodrigoV <jaicelvelasco105@gmail.com>
2025-04-17 21:09:03 -0400

    config: actualizar la configuracion de proyecto en angular.json y tsconfig.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { authGuard } from "./core/guards/auth.guard";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "login",
+    loadComponent: () =>
+      import("./features/auth/login/login.component").then(
+        (m) => m.LoginComponent,
+      ),
+  },
+  {
+    path: "admin",
+    loadChildren: () =>
+      import("./features/admin/admin.routes").then((m) => m.adminRoutes),
+    canActivate: [authGuard],
+    data: { requiredRole: Rol.ADMIN },
+  },
+  {
+    path: "curator",
+    loadChildren: () =>
+      import("./features/curator/curator.routes").then((m) => m.curatorRoutes),
+    canActivate: [authGuard],
+    data: { requiredRole: Rol.CURATOR },
+  },
+];
