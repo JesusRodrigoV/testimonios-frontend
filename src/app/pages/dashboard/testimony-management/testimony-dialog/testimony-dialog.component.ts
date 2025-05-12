f05b1b05faef1dd8792b3b889f46df3a0c762953
JesusRodrigoV <jaicelvelasco105@gmail.com>
2025-05-12 10:19:34 -0400

    fix: Arreglos de formato de video

--- src/app/pages/dashboard/testimony-management/testimony-dialog/testimony-dialog.component.ts
+++ src/app/pages/dashboard/testimony-management/testimony-dialog/testimony-dialog.component.ts
@@ -2,11 +2,12 @@ import { DatePipe } from "@angular/common";
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { VideoPlayerComponent } from "@app/features/shared/video-player";
 import { Testimony } from "@app/features/testimony/models/testimonio.model";
 
 @Component({
   selector: "app-testimony-dialog",
-  imports: [DatePipe, MatButtonModule],
+  imports: [DatePipe, MatButtonModule, VideoPlayerComponent],
   templateUrl: "./testimony-dialog.component.html",
   styleUrl: "./testimony-dialog.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
