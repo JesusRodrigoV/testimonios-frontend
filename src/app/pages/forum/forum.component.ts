607095b9c78da487269fab3a9763731f86f06025
JesusRodrigoV <jaicelvelasco105@gmail.com>
2025-06-02 17:38:45 -0400

    fix: Arreglando foros

--- src/app/pages/forum/forum.component.ts
+++ src/app/pages/forum/forum.component.ts
@@ -1,19 +1,27 @@
-import { AsyncPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { RouterLink } from '@angular/router';
-import { ForumPostComponent } from '@app/features/forum/components/forum-post';
-import { ForumService } from '@app/features/forum/services';
+import { AsyncPipe } from "@angular/common";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { RouterLink } from "@angular/router";
+import { ForumPostComponent } from "@app/features/forum/components/forum-post";
+import { ForumService } from "@app/features/forum/services";
 
 @Component({
-  selector: 'app-forum',
-  imports: [AsyncPipe, ForumPostComponent, RouterLink, MatDividerModule, MatIconModule, MatButtonModule, MatTooltipModule],
-  templateUrl: './forum.component.html',
-  styleUrl: './forum.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-forum",
+  imports: [
+    AsyncPipe,
+    ForumPostComponent,
+    RouterLink,
+    MatDividerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+  ],
+  templateUrl: "./forum.component.html",
+  styleUrl: "./forum.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class ForumComponent {
   private forumService = inject(ForumService);
